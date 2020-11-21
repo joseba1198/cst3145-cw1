9fc4908595679d2326541a2a0bf52318a9a6b133
joseba vazquez <joseph.yoyoitsjojo123@gmail.com>
2020-11-21 14:09:17 +0000

    Created Products In JSON

---
+++ products.js
@@ -0,0 +1,137 @@
+let products = [
+
+ 
+
+    {
+
+        "id": 1001,
+        "title": "Maths",
+        "description": "test description",
+        "price": 20,
+        "image":"maths.jpg",
+        "availableInventory": 5,
+        "rating": 1
+    
+    },
+    
+    {
+    
+        "id": 1002,
+        "title": "English",
+        "description": "test description",
+        "price": 30,
+        "image":"science.jpg",
+        "availableInventory": 5
+        
+    },
+    
+    {
+    
+        "id": 1003,
+        "title": "French",
+        "description": "test description",
+        "price": 30,
+        "image":"maths.jpg",
+        "availableInventory": 5
+        
+    },
+    
+    {
+    
+        "id": 1004,
+        "title": "Spanish",
+        "description": "test description",
+        "price": 15,
+        "image":"maths.jpg",
+        "availableInventory": 5
+        
+    },
+    
+    {
+    
+        "id": 1005,
+        "title": "ICT",
+        "description": "test description",
+        "price": 10,
+        "image":"maths.jpg",
+        "availableInventory": 5
+        
+    },
+    
+    {
+    
+        "id": 1006,
+        "title": "Maths",
+        "description": "test description",
+        "price": 20,
+        "image":"maths.jpg",
+        "availableInventory": 5
+        
+    },
+    
+    {
+    
+        "id": 1007,
+        "title": "Maths",
+        "description": "test description",
+        "price": 20,
+        "image":"maths.jpg",
+        "availableInventory": 5
+        
+    },
+    
+    {
+    
+        "id": 1008,
+        "title": "Maths",
+        "description": "test description",
+        "price": 20,
+        "image":"maths.jpg",
+        "availableInventory": 5
+        
+    },
+    
+    {
+    
+        "id": 1009,
+        "title": "Maths",
+        "description": "test description",
+        "price": 20,
+        "image":"maths.jpg",
+        "availableInventory": 5
+        
+    },
+    
+    {
+    
+        "id": 1010,
+        "title": "Maths",
+        "description": "test description",
+        "price": 20,
+        "image":"maths.jpg",
+        "availableInventory": 5
+        
+    },
+    
+    {
+    
+        "id": 1011,
+        "title": "Maths",
+        "description": "test description",
+        "price": 20,
+        "image":"maths.jpg",
+        "availableInventory": 5
+        
+    },
+    
+    {
+    
+        "id": 1012,
+        "title": "Maths",
+        "description": "test description",
+        "price": 20,
+        "image":"maths.jpg",
+        "availableInventory": 5
+        
+    },
+    ]
